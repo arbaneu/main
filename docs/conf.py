b1bf23e76ad6fb344382f7fd8831afd4cfa19058
Yuta Katsumi <yutakatsumi@Yutas-MacBook-Air.local>
2019-01-27 23:47:22 -0500

    Initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'arba'
+project = 'ARBA'
 copyright = '2019, Yuta Katsumi'
 author = 'Yuta Katsumi'
 
@@ -83,7 +83,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
